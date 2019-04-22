0d20c8f9d2d2de9bb2d8aa7fc3b1d49e9640e960
Oli Gorges <oligorges@gmail.com>
2019-04-22 23:32:04 +0300

    add DB to endpoints

--- service/controllers/config.js
+++ service/controllers/config.js
@@ -1,23 +1,47 @@
 
 const model = require('../models/configModel').Model
 const crud = require('../middelware/CRUD')
+const crypto = require('crypto');
+const sharp = require('sharp')
+const multer= require('multer')
 const db = new crud()
 module.exports = function(app) {
 
-    function addDBContext(req, res, next){
-        req.DBModel = model
-        next()
-    }
+    +
 
-    app.get('/', addDBContext , db.read, (req, res) => {
-        res.send(req.body)
+    app.get('/', (req, res) => {
+        model.find({}, (err, data) => {
+            if (err){
+                res.send({msg:'Can`t find Object'})
+            } else{
+                res.send(data)
+            }
+            
+        })
     })
 
-    app.patch('/:key', addDBContext, db.update, (req, res)=>{
-        res.sendStatus(200)
+    app.patch('/:key', (req, res)=>{
+        model.updateOne({Key: req.params.key}, req.body, (err, data) => {
+            if (err){
+                res.send({msg:'Can`t find Object'})
+            } else{
+                res.send(data)
+            }
+            
+        })
     })
 
-    app.post('/logo',  (req, res)=>{
-
+    app.patch('/password',  (req, res)=>{
+        const hash = crypto.createHmac('sha256', process.env.salt)
+        .update(req.body.password)
+        .digest('hex');
+        process.env.password = hash
     })
+
+    app.post('/logo', multer.single('image'), (req, res)=>{
+        sharp(req.file).resize({ height: 500 }).toFile('./public/logo.png', (err, info) => { 
+            console.log(err, info)
+         })
+    })
+    
 }
