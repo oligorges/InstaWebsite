0415e144a24275727e866232466ff76a0d7d6898
Oli Gorges <oligorges@gmail.com>
2019-04-26 08:06:21 +0300

    fix config

--- config.js
+++ config.js
@@ -8,11 +8,12 @@ const dev = {
         DBPort: process.env.DBPORT || 27017,
         DBTable: process.env.DBTable || 'InstPort',
         DBUrl: process.env.DBUrl || '127.0.0.1',
+        DBUser: process.env.DBUser || '',
+        DBPW: process.env.DBPW || '',
         DistPath: process.env.distPath || './public/',
         insta:{
             clientId: process.env.IGClientID || '',
             clientSecret: process.env.IGSecret || '',
-            token: process.env.IGToken || ''
         },
         settings: [
             {  Key: 'Color', Value: process.env.Color || '#000', Public: true},
@@ -20,6 +21,7 @@ const dev = {
             {  Key: 'Password', Value: process.env.Password || 'bec00353fe51634a2c28934b21d1e557dae7b32443e5929c4797dcd587d2dda9', Public: false},
             {  Key: 'IGKey', Value: process.env.IGKey || '', Public: false},
             {  Key: 'IGLink', Value: process.env.IGLink || 'https://www.instagram.com/oli.gorges/', Public: true},
+            {  Key: 'TagRegex', Value: process.env.TagRegex || '_.*_', Public: false},
             {  Key: 'Logo', Value: process.env.Logo || '/public/logo.png', Public: true },
             {  Key: 'TitleImage', Value: process.env.HeadImg || 'https://i.kinja-img.com/gawker-media/image/upload/s--kHrQ8nr7--/c_scale,f_auto,fl_progressive,q_80,w_800/18huxz4bvnfjbjpg.jpg', Public: true },
             {  Key: 'Intro', Value: process.env.WelcomeText || 'Welcome to my Portfolio', Public: true },
