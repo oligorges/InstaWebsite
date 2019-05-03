29f4f36f6a0202bb17dafc65ee5af01f2388d41a
Oli Gorges <oligorges@gmail.com>
2019-05-04 02:06:19 +0300

    update cinfig

--- service/helper/initConfig.js
+++ service/helper/initConfig.js
@@ -1,12 +1,13 @@
-const model = require('../models/configModel').Model
-const defaultConf = require('../../config').server
 
-model.collection.drop().then(()=>{
-    console.log('deleted Table')
-    return model.createCollection()
-}).then(() => {
+const db = require('mongoose')
+const model = require('../models/configModel').Model
+const config = require('../../config')
+
+db.connect(`mongodb://${config.server.DBUser}:${config.server.DBPW}@${config.server.DBUrl}:${config.server.DBPort}/${config.server.DBTable}`, {useNewUrlParser: true})
+
+model.createCollection().then(() => {
     console.log('crated Table')
-    return model.create(defaultConf.settings)
+    return model.create(config.server.settings)
 }).then(()=>{
     console.log('Init new Settings')
 }).catch((err)=>{
